ab811164a473a95242a2f52291720a3c9c0efe1a
GolfGrab <surapus01@gmail.com>
2024-12-01 22:09:30 +0700

    chore: Update main.ts imports and remove unused code

--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,19 +1,13 @@
 import { ParseJsonPipe } from '@/pipe/ParseJson.pipe';
 import { loggerOption } from '@/utils/logger/loggerOption';
-import { generateSwaggerDoc } from '@/utils/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { WinstonModule } from 'nest-winston';
-import { PrismaClientExceptionFilter } from 'nestjs-prisma';
-import { AppModule } from './app.module';
-import { generateModuleGraph } from './utils/module-graph';
+import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { Config } from './utils/config/config-dto';
 import { TypedConfigModule, dotenvLoader } from 'nest-typed-config';
+import { WinstonModule } from 'nest-winston';
+import { AppModule } from './app.module';
+import { Config } from './utils/config/config-dto';
+import { generateModuleGraph } from './utils/module-graph';
 
 async function bootstrap() {
   const logger = WinstonModule.createLogger(loggerOption);
