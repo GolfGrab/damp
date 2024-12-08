2220a3677cfcabd68f1d09bcc2454adc1c7e4911
Katid Supawee <katidd17@gmail.com>
2024-12-08 19:01:56 +0700

    feat(template-engine): add text editor component for test

--- frontend/src/modules/template-engine/rich-text-editor/RichTextEditor.tsx
+++ frontend/src/modules/template-engine/rich-text-editor/RichTextEditor.tsx
@@ -1,29 +1,7 @@
 import { useClient } from "@/modules/common/hooks/useClient";
-import { Blockquote } from "@tiptap/extension-blockquote";
-import { Bold } from "@tiptap/extension-bold";
-import { BulletList } from "@tiptap/extension-bullet-list";
-import { Code } from "@tiptap/extension-code";
-import { CodeBlock } from "@tiptap/extension-code-block";
-import { Color } from "@tiptap/extension-color";
-import { Document } from "@tiptap/extension-document";
-import { HardBreak } from "@tiptap/extension-hard-break";
-import { Highlight } from "@tiptap/extension-highlight";
-import { History } from "@tiptap/extension-history";
-import { HorizontalRule } from "@tiptap/extension-horizontal-rule";
-import { Italic } from "@tiptap/extension-italic";
-import { Link } from "@tiptap/extension-link";
-import { ListItem } from "@tiptap/extension-list-item";
-import { OrderedList } from "@tiptap/extension-ordered-list";
-import { Paragraph } from "@tiptap/extension-paragraph";
-import { Placeholder } from "@tiptap/extension-placeholder";
-import { Strike } from "@tiptap/extension-strike";
-import { Text } from "@tiptap/extension-text";
-import { TextAlign } from "@tiptap/extension-text-align";
-import { TextStyle } from "@tiptap/extension-text-style";
-import { useEditor } from "@tiptap/react";
+import { type Editor } from "@tiptap/react";
 import {
   LinkBubbleMenu,
-  LinkBubbleMenuHandler,
   MenuButtonBlockquote,
   MenuButtonBold,
   MenuButtonBulletedList,
@@ -47,53 +25,11 @@ import {
   RichTextField,
 } from "mui-tiptap";
 
-export default function RichTextEditor() {
-  const CustomLinkExtension = Link.extend({
-    inclusive: false,
-  });
-  const extensions = [
-    // Config editor
-    Placeholder.configure({
-      placeholder: "Add your own content here...",
-    }),
-    History,
+type RichTextEditorProps = {
+  editor: Editor;
+};
 
-    // common features
-    Text,
-    Paragraph,
-    Document,
-    BulletList,
-    ListItem,
-    OrderedList,
-    HardBreak,
-    CustomLinkExtension.configure({
-      autolink: true,
-      linkOnPaste: true,
-      openOnClick: false,
-    }),
-    LinkBubbleMenuHandler,
-
-    // Markdown, HTML features
-    Bold,
-    Italic,
-    Strike,
-    Code,
-    CodeBlock,
-    Blockquote,
-    HorizontalRule,
-
-    // HTML only features
-    TextAlign.configure({
-      types: ["heading", "paragraph", "image"],
-    }),
-    TextStyle,
-    Color,
-    Highlight.configure({ multicolor: true }),
-  ];
-  const editor = useEditor({
-    extensions: extensions,
-    content: "<p>Hello <b>world</b>!</p>",
-  });
+export default function RichTextEditor({ editor }: RichTextEditorProps) {
   const { isClientLoaded } = useClient();
   if (!isClientLoaded) return null;
   return (
