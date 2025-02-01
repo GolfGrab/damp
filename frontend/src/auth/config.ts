c90f96dee80a10cde71c3ee4d39195e57718c110
GolfGrab <surapus01@gmail.com>
2025-02-01 18:07:22 +0700

    chore: Update OIDC configuration and axios request interceptor

--- frontend/src/auth/config.ts
+++ frontend/src/auth/config.ts
@@ -1,6 +1,7 @@
-import { WebStorageStateStore } from "oidc-client-ts";
+import { User, WebStorageStateStore } from "oidc-client-ts";
 import { AuthProviderProps } from "react-oidc-context";
 import { Location, NavigateFunction } from "react-router-dom";
+import { axiosInstance } from "../api";
 
 export const getOidcConfig = (
   navigator: NavigateFunction
@@ -10,9 +11,25 @@ export const getOidcConfig = (
   redirect_uri: window.location.origin + "/auth/callback",
   scope: "openid email groups profile",
   onSigninCallback: (user): void => {
-    navigator((user?.state as { returnTo: Location }).returnTo ?? "/");
+    navigator((user?.state as { returnTo: Location })?.returnTo ?? "/");
   },
   userStore: new WebStorageStateStore({ store: window.localStorage }),
-  stateStore: new WebStorageStateStore({ store: sessionStorage }),
+  stateStore: new WebStorageStateStore({ store: window.localStorage }),
   automaticSilentRenew: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const oidcStorage = localStorage.getItem(
+    `oidc.user:https://api.asgardeo.io/t/damp/oauth2/token:TRnCWdQg0YXiEzWECZ3S_hkkfpwa`
+  );
+  if (!oidcStorage) {
+    return config;
+  }
+
+  const user = User.fromStorageString(oidcStorage);
+
+  if (user && user.access_token) {
+    config.headers.Authorization = `Bearer ${user.access_token}`;
+  }
+  return config;
 });
